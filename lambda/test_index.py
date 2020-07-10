e3f99cb296228a9c1d3b5ba714791fb4ff8a5bb8
tnielsen2 <trentnielsen84@gmail.com>
2020-07-10 14:31:01 -0400

    Assert the put, assert the update

--- lambda/test_index.py
+++ lambda/test_index.py
@@ -1,5 +1,6 @@
 import unittest
 import boto3
+import random
 from moto import mock_dynamodb2
 from . import index
 
@@ -11,9 +12,26 @@ class TestDynamo(unittest.TestCase):
     def test_get_visitors_counter(self):
         # Set up a mock dynamodb using ddbmock
         tablename = 'unittest'
+        randominterger = random.randint(1, 100)
+        # Create mock dynamodb connection
         dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+        # Create table
         index.create_dynamo_table(tablename, dynamodb)
+        print('Table "{}" successfully created.'.format(tablename))
+        # Put default value into the table
         index.put_table_counters(tablename, 0, dynamodb)
+        print('Inserted value of {} into the table'.format(randominterger))
+        # Get the value of the table
         result = index.get_visitors_counter(tablename, dynamodb)
-        print(result)
-        self.assertIsNotNone(result)
+        print('Retreived value of {} from table {}'.format(result, tablename))
+        # Assert the result of the put matches the random integer
+        self.assertEqual(randominterger, result)
+        # Update the table
+        index.update_table_counters(tablename, randominterger + 1, dynamodb)
+        # Get the new value
+        result = index.get_visitors_counter(tablename, dynamodb)
+        print('Retreived updated value of {} from table {}'.format(result, tablename))
+        self.assertEqual(randominterger + 1, result)
+
+
+
